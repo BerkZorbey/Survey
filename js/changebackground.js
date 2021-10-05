ce030d1a5044d8989bd7528d2e3613defb3cde80
BerkZorbey <berktaha1994@gmail.com>
2021-10-06 01:30:48 +0300

    Checkboxlardaki num ve disable sorunu çözüldü

--- js/changebackground.js
+++ js/changebackground.js
@@ -1,5 +1,40 @@
 var num=0;
-
+var i1=0;
+var i2=0;
+var num3_1=0;
+var num3_2=0;
+var num3_3=0;
+var num3_4=0;
+var num3_5=0;
+var num3_6=0;
+var i4=0;
+var num5_1=0;
+var num5_2=0;
+var num5_3=0;
+var num5_4=0;
+var num5_5=0;
+var i6=0;
+var i7=0;
+var i8=0;
+var num8_1=0;
+var num8_2=0;
+var num8_3=0;
+var i9=0;
+var i10=0;
+var i11=0;
+var i12=0;
+var num12_1=0;
+var num12_2=0;
+var num12_3=0;
+var i13=0;
+var i14=0;
+var num14_1=0;
+var num14_2=0;
+var num14_3=0;
+var num14_4=0;
+var i15=0;
+var i18=0;
+var i19=0;
 var url= 'url("image/sky.jpg")';
 var url1= 'url("image/sky2.jpg")';
 var url2='url("image/sky3.jpg")';
@@ -12,62 +47,193 @@ function disablecheckbox_3(){
     var soru3_3=document.getElementById("soru3.3").disabled =true;
     var soru3_4=document.getElementById("soru3.4").disabled =true;
     var soru3_5=document.getElementById("soru3.5").disabled =true;
+    
     num+=6;
-  return  soru3_1 + soru3_2 +soru3_3 + soru3_4 +soru3_5;}
-  else if(document.getElementById("soru3.1").checked==true||document.getElementById("soru3.2").checked==true||document.getElementById("soru3.3").checked==true||document.getElementById("soru3.4").checked==true||document.getElementById("soru3.5").checked==true){
+    num3_6=1;
+    return  soru3_1 + soru3_2 +soru3_3 + soru3_4 +soru3_5;
+        }
+    
+    else if(document.getElementById("soru3.6").checked==false && num3_6==1){
+        num-=6;
+        num3_6=0;
+        soru3_1=document.getElementById("soru3.1").disabled =false;
+        soru3_2=document.getElementById("soru3.2").disabled =false;
+        soru3_3=document.getElementById("soru3.3").disabled =false;
+        soru3_4=document.getElementById("soru3.4").disabled =false;
+        soru3_5=document.getElementById("soru3.5").disabled =false;
+       
+        return soru3_1 + soru3_2 +soru3_3 + soru3_4 +soru3_5;
+        }
+
+  else if(document.getElementById("soru3.1").checked==true && num3_1==0){
       num-=1;
-      return document.getElementById("soru3.6").disabled=true;
-      
+      num3_1=1;
+    document.getElementById("soru3.6").disabled = true;
   }
-  else{
-    soru3_1=document.getElementById("soru3.1").disabled =false;
-    soru3_2=document.getElementById("soru3.2").disabled =false;
-    soru3_3=document.getElementById("soru3.3").disabled =false;
-    soru3_4=document.getElementById("soru3.4").disabled =false;
-    soru3_5=document.getElementById("soru3.5").disabled =false;
-    soru3_6=document.getElementById("soru3.6").disabled=false;
-    return soru3_1 + soru3_2 +soru3_3 + soru3_4 +soru3_5+soru3_6;
+  else if(document.getElementById("soru3.1").checked==false && num3_1==1){
+    num+=1;
+    num3_1=0;  
+     if(document.getElementById("soru3.1").checked==false && document.getElementById("soru3.2").checked==false && document.getElementById("soru3.3").checked==false && document.getElementById("soru3.4").checked==false && document.getElementById("soru3.5").checked==false){
+    
+        document.getElementById("soru3.6").disabled=false;
+        
+    }
   }
+
+  else if(document.getElementById("soru3.2").checked==true && num3_2==0){
+    num-=1;
+    num3_2=1;
+    document.getElementById("soru3.6").disabled = true;
+}
+else if(document.getElementById("soru3.2").checked==false && num3_2==1){
+  num+=1;
+  num3_2=0;  
+ if(document.getElementById("soru3.1").checked==false && document.getElementById("soru3.2").checked==false && document.getElementById("soru3.3").checked==false && document.getElementById("soru3.4").checked==false && document.getElementById("soru3.5").checked==false){
+    
+    document.getElementById("soru3.6").disabled=false;
+    
+}
+  
+}
+
+else if(document.getElementById("soru3.3").checked==true && num3_3==0){
+    num-=1;
+    num3_3=1;
+    document.getElementById("soru3.6").disabled = true;
+}
+else if(document.getElementById("soru3.3").checked==false && num3_3==1){
+  num+=1;
+  num3_3=0;  
+  if(document.getElementById("soru3.1").checked==false && document.getElementById("soru3.2").checked==false && document.getElementById("soru3.3").checked==false && document.getElementById("soru3.4").checked==false && document.getElementById("soru3.5").checked==false){
+    
+    document.getElementById("soru3.6").disabled=false;
+    
+}
+}
+
+else if(document.getElementById("soru3.4").checked==true && num3_4==0){
+    num-=1;
+    num3_4=1;
+    document.getElementById("soru3.6").disabled = true;
+}
+else if(document.getElementById("soru3.4").checked==false && num3_4==1){
+  num+=1;
+  num3_4=0;  
+  if(document.getElementById("soru3.1").checked==false && document.getElementById("soru3.2").checked==false && document.getElementById("soru3.3").checked==false && document.getElementById("soru3.4").checked==false && document.getElementById("soru3.5").checked==false){
+    
+    document.getElementById("soru3.6").disabled=false;
+    
+}
+}
+
+else if(document.getElementById("soru3.5").checked==true && num3_5==0){
+    num-=1;
+    num3_5=1;
+    document.getElementById("soru3.6").disabled = true;
+}
+else if(document.getElementById("soru3.5").checked==false && num3_5==1){
+  num+=1;
+  num3_5=0;  
+  if(document.getElementById("soru3.1").checked==false && document.getElementById("soru3.2").checked==false && document.getElementById("soru3.3").checked==false && document.getElementById("soru3.4").checked==false && document.getElementById("soru3.5").checked==false){
+    
+    document.getElementById("soru3.6").disabled=false;
+    
+}
+}
+
+  
 }
 //5.sorudaki checkbox disable etme fonksiyonu
 function disablecheckbox_5() {
-    if(document.getElementById("soru5.1").checked==true || document.getElementById("soru5.2").checked==true || document.getElementById("soru5.3").checked==true){
+    if(document.getElementById("soru5.1").checked==true && num5_1==0){
         var soru5_4=document.getElementById("soru5.4").disabled =true;
         var soru5_5=document.getElementById("soru5.5").disabled =true;
         num -=2;
-        
+        num5_1=1;
       return  soru5_4 + soru5_5;}
-      else if(document.getElementById("soru5.4").checked==true||document.getElementById("soru5.5").checked==true){
+      else if(document.getElementById("soru5.1").checked==false && num5_1==1){
+          num+=2;
+          num5_1=0;
+
+           if(document.getElementById("soru5.1").checked==false && document.getElementById("soru5.2").checked==false && document.getElementById("soru5.3").checked==false){
+            var soru5_4=document.getElementById("soru5.4").disabled =false;
+            var soru5_5=document.getElementById("soru5.5").disabled =false;
+            return  soru5_4 + soru5_5;
+          }
+      }
+      else if(document.getElementById("soru5.2").checked==true && num5_2==0){
+        var soru5_4=document.getElementById("soru5.4").disabled =true;
+        var soru5_5=document.getElementById("soru5.5").disabled =true;
+        num -=2;
+        num5_2=1;
+      return  soru5_4 + soru5_5;}
+      else if(document.getElementById("soru5.2").checked==false && num5_2==1){
+          num+=2;
+          num5_2=0;
+          if(document.getElementById("soru5.1").checked==false && document.getElementById("soru5.2").checked==false && document.getElementById("soru5.3").checked==false){
+            var soru5_4=document.getElementById("soru5.4").disabled =false;
+            var soru5_5=document.getElementById("soru5.5").disabled =false;
+            return  soru5_4 + soru5_5;
+          }
+      }
+      else if(document.getElementById("soru5.3").checked==true && num5_3==0){
+        var soru5_4=document.getElementById("soru5.4").disabled =true;
+        var soru5_5=document.getElementById("soru5.5").disabled =true;
+        num -=2;
+        num5_3=1;
+      return  soru5_4 + soru5_5;}
+      else if(document.getElementById("soru5.3").checked==false && num5_3==1){
+          num+=2;
+          num5_3=0;
+          if(document.getElementById("soru5.1").checked==false && document.getElementById("soru5.2").checked==false && document.getElementById("soru5.3").checked==false){
+            var soru5_4=document.getElementById("soru5.4").disabled =false;
+            var soru5_5=document.getElementById("soru5.5").disabled =false;
+            return  soru5_4 + soru5_5;
+          }
+      }
+
+      else if(document.getElementById("soru5.4").checked==true && num5_4==0){
         var soru5_1=document.getElementById("soru5.1").disabled =true;
         var soru5_2=document.getElementById("soru5.2").disabled =true;
         var soru5_3=document.getElementById("soru5.3").disabled =true;
         num +=3;
+        num5_4=1;
         return  soru5_1 + soru5_2 + soru5_3;
     }
-    else{
-        soru5_1=document.getElementById("soru5.1").disabled =false;
-        soru5_2=document.getElementById("soru5.2").disabled =false;
-        soru5_3=document.getElementById("soru5.3").disabled =false;
-        soru5_4=document.getElementById("soru5.4").disabled =false;
-        soru5_5=document.getElementById("soru5.5").disabled =false;
+    else if(document.getElementById("soru5.4").checked==false && num5_4==1){
         
-        return soru5_1 + soru5_2 +soru5_3 + soru5_4 +soru5_5;}
-}
-var i1=0;
-var i2=0;
-var i4=0;
-var i6=0;
-var i7=0;
-var i8=0;
-var i9=0;
-var i10=0;
-var i11=0;
-var i12=0;
-var i13=0;
-var i14=0;
-var i15=0;
-var i18=0;
-var i19=0;
+        num -=3;
+        num5_4=0;
+        if(document.getElementById("soru5.4").checked==false && document.getElementById("soru5.5").checked==false){
+            var soru5_4=document.getElementById("soru5.1").disabled =false;
+            var soru5_5=document.getElementById("soru5.2").disabled =false;
+            var soru5_5=document.getElementById("soru5.3").disabled =false;
+            return  soru5_1 + soru5_2 + soru5_3;
+          }
+      
+    }
+    else if(document.getElementById("soru5.5").checked==true && num5_5==0){
+        var soru5_1=document.getElementById("soru5.1").disabled =true;
+        var soru5_2=document.getElementById("soru5.2").disabled =true;
+        var soru5_3=document.getElementById("soru5.3").disabled =true;
+        num +=3;
+        num5_5=1;
+        return  soru5_1 + soru5_2 + soru5_3;
+    }
+    else if(document.getElementById("soru5.5").checked==false && num5_5==1){
+        
+        num -=3;
+        num5_5=0;
+        if(document.getElementById("soru5.4").checked==false && document.getElementById("soru5.5").checked==false){
+            var soru5_4=document.getElementById("soru5.1").disabled =false;
+            var soru5_5=document.getElementById("soru5.2").disabled =false;
+            var soru5_5=document.getElementById("soru5.3").disabled =false;
+            return  soru5_1 + soru5_2 + soru5_3;
+          }
+      
+    }
+}   
+
  $('.form-radio').change(function() {
      
     //1.soru
@@ -177,26 +343,64 @@ var i19=0;
         
     }
 
-    //8.soru hatalı
+    //8.soru 
     else if(this.id=="soru8.1"){
+        num8_1=0;
         if(i8==0){
             num+=5;
            i8=1;
+           num8_1=1;
            }
-           else if(i8==1){
-              num+=10; 
-           } 
+           else if(num8_2==1){
+            num+=3;
+            num8_1=1;
+            num8_2=0;
+        } 
+        else if(num8_3==1){
+            num+=10;
+            num8_1=1;
+            num8_3=0;
+        }  
+           
+        
+    }
+    else if(this.id=="soru8.2"){
+        num8_2=0;
+        if(i8==0){
+            num+=2;
+           i8=1;
+           num8_2=1;
+           }
+          else if(num8_1==1){
+              num-=3;
+              num8_2=1;
+              num8_1=0;
+          } 
+          else if(num8_3==1){
+            num+=7;
+            num8_2=1;
+            num8_3=0;
+        }  
         
     }
     
     else if(this.id=="soru8.3"){
+        num8_3=0;
         if(i8==0){
             num-=5;
            i8=1;
+           num8_3=1;
            }
-           else if(i8==1){
-              num-=10; 
-           } 
+           else if(num8_1==1){
+            num-=10;
+            num8_3=1;
+            num8_1=0;
+        }  
+        else if(num8_2==1){
+            num-=7;
+            num8_3=1;
+            num8_2=0;
+        } 
         
     }
 
@@ -263,26 +467,63 @@ var i19=0;
            } 
        
     }
-    //12.soru hatalı
+    //12.soru 
     else if(this.id=="soru12.1"){
+        num12_1=0;
         if(i12==0){
             num-=5;
            i12=1;
+           num12_1=1;
            }
-           else if(i12==1){
-              num-=10; 
+           else if(num12_2==1){
+            num-=7;
+            num12_1=1;
+            num12_2=0; 
+         } 
+         else if(num12_3==1){
+           num-=10;  
+          num12_1=1;
+          num12_3=0;
+         }
+       
+    }
+    else if(this.id=="soru12.2"){
+        num12_2=0;
+        if(i12==0){
+            num+=2;
+           i12=1;
+           num12_2=1;
+           }
+           else if(num12_1==1){
+              num+=7;
+              num12_2=1;
+              num12_1=0; 
            } 
+           else if(num12_3==1){
+               num-=3;
+            num12_2=1;
+            num12_3=0;
+           }
        
     }
     
     else if(this.id=="soru12.3"){
+        num12_3=0;
         if(i12==0){
             num+=5;
            i12=1;
+           num12_3=1;
            }
-           else if(i12==1){
-              num+=10; 
-           } 
+           else if(num12_1==1){
+            num+=10;
+            num12_3=1;
+            num12_1=0; 
+         } 
+         else if(num12_2==1){
+             num+=3;
+          num12_3=1;
+          num12_2=0;
+         }
        
     }
     //13.soru
@@ -307,45 +548,113 @@ var i19=0;
        
     }
     
-    //14.soru hatalı
+    //14.soru 
     else if(this.id=="soru14.1"){
+        num14_1=0;
         if(i14==0){
             num+=10;
            i14=1;
+           num14_1=1;
            }
-           else if(i14==1){
-              num+=20; 
+           else if(num14_2==1){
+               num+=5;
+               num14_2=0;
+               num14_1=1;
+               
            } 
+           else if(num14_3==1){
+               num+=8;
+            num14_3=0;
+            num14_1=1;
+            
+         } 
+         else if(num14_4==1){
+             num+=15;
+            num14_4=0;
+            num14_1=1;
+             
+         } 
         
     }
     else if(this.id=="soru14.2"){
+        num14_2=0;
         if(i14==0){
             num+=5;
            i14=1;
+           num14_2=1;
            }
-           else if(i14==1){
-              num+=10; 
-           } 
+           else if(num14_1==1){
+               num-=5;
+            num14_1=0;
+            num14_2=1;
+            
+        } 
+        else if(num14_3==1){
+            num+=3;
+         num14_3=0;
+         num14_2=1;
+         
+      } 
+      else if(num14_4==1){
+          num+=10;
+         num14_4=0;
+         num14_2=1;
+          
+      } 
         
     }
     else if(this.id=="soru14.3"){
+        num14_3=0;
         if(i14==0){
             num+=2;
            i14=1;
+           num14_3=1;
            }
-           else if(i14==1){
-              num+=4; 
-           } 
+           else if(num14_2==1){
+               num-=3;
+            num14_2=0;
+            num14_3=1;
+            
+        } 
+        else if(num14_1==1){
+            num-=8;
+         num14_1=0;
+         num14_3=1;
+         
+      } 
+      else if(num14_4==1){
+          num+=7;
+         num14_4=0;
+         num14_3=1;
+          
+      } 
         
     }
     else if(this.id=="soru14.4"){
+        num14_4=0;
         if(i14==0){
             num-=5;
            i14=1;
+           num14_4=1;
            }
-           else if(i14==1){
-              num-=10; 
-           } 
+           else if(num14_2==1){
+               num-=10;
+            num14_2=0;
+            num14_4=1;
+            
+        } 
+        else if(num14_3==1){
+            num-=7;
+         num14_3=0;
+         num14_4=1;
+         
+      } 
+      else if(num14_1==1){
+          num-=15;
+         num14_1=0;
+         num14_4=1;
+          
+      } 
         
     }
     //15.soru
